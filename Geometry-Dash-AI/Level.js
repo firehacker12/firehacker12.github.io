265570a35eacaa859a4b6ea1d3512974ead44411
firehacker12 <halestorm512@gmail.com>
2018-07-29 19:46:29 -0400

    Add files via upload

---
+++ Geometry-Dash-AI/Level.js
@@ -0,0 +1,4 @@
+function Ground() {
+	fill(0);
+	rect(0, height/2+200, width, 20);
+}
---
+++ Geometry-Dash-AI/Main.js
@@ -0,0 +1,8 @@
+function setup() {
+	createCanvas(600, 600);
+}
+
+function draw() {
+	background(37, 107, 219);
+	Ground();
+}
