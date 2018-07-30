7c4a48ca5794eeb79e663a455baf096250008d1d
firehacker12 <halestorm512@gmail.com>
2018-07-29 20:15:05 -0400

    Update Main.js

--- Geometry-Dash-AI/Main.js
+++ Geometry-Dash-AI/Main.js
@@ -1,8 +1,20 @@
+var Player;
+
 function setup() {
-	createCanvas(600, 600);
+	createCanvas(6000, 600);
+	Player = new PlayerFunction();
 }
 
 function draw() {
 	background(37, 107, 219);
 	Ground();
+	Player.show();
+	Player.update();
+}
+
+function keyPressed() {
+	if (keyCode === 32) {
+		Player.jump();
+		console.log("Jumped");
+	}
 }
