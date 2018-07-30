fb3bfc2f32430f40dc4ecb22ae1f38d67d09add4
firehacker12 <halestorm512@gmail.com>
2018-07-29 20:14:43 -0400

    Update Player.js

--- Geometry-Dash-AI/Player.js
+++ Geometry-Dash-AI/Player.js
@@ -1,3 +1,29 @@
-function Player() {
-	
+function PlayerFunction() {
+	var Jumping = false;
+	this.x = 30;
+	this.y = height/2+160;
+	var Gravity = 1;
+	var Velocity = 1;
+	this.show = function() {
+		fill(200, 0, 0);
+		rect(this.x, this.y, 40, 40);
+	}
+	this.jump = function() {
+		if (!Jumping) {
+			Velocity = -12;
+		}
+	}
+	this.update = function() {
+		this.y -= -Gravity-Velocity;
+		if (this.y >= height/2+160) {
+			this.y = height/2+160;
+		}
+		if (this.y == height/2+160) {
+			Jumping = false;
+		}
+		else {
+			Jumping = true;
+		}
+		Velocity+=0.5;
+	}
 }
