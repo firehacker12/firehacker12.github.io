72da68934b30471ef32b4a5f218a0d48858b338a
firehacker12 <halestorm512@gmail.com>
2018-07-31 19:17:24 -0400

    Update Level.js

--- Geometry-Dash-AI/Level.js
+++ Geometry-Dash-AI/Level.js
@@ -5,25 +5,25 @@ function Ground() {
 
 var Level = [];
 
-
+var LevelLength = 20;
 
 function LevelFunction() {
 	var Spikes = [];
 	var Speed = 5;
 	var Scale = 40;
 	this.generateSpikes = function(NumberOfSpikes, x, y, Distance) {
-		for (var i=0; i<Level.length; i++) {
+		for (var i=0; i<LevelLength; i++) {
 			Spikes[i] = createVector(x, y);
 			x+=Scale;
 		}
 	}
 	this.moveLevel = function() {
-		for (var i=0; i<Spikes.length; i++) {
+		for (var i=0; i<LevelLength; i++) {
 			Spikes[i].x -= Speed;
 		}
 	}
 	this.show = function() {
-		for (var i=0; i<Spikes.length; i++) {
+		for (var i=0; i<LevelLength; i++) {
 			fill(100, 0, 100);
 			if (Level[i] == '1') {
 				triangle(Spikes[i].x, Spikes[i].y, Spikes[i].x+40, Spikes[i].y, Spikes[i].x+20, Spikes[i].y-30);
