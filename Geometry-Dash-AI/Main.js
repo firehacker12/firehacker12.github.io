0fe1301fd9561a6e97cdb0dc8aed773c89724b0b
firehacker12 <halestorm512@gmail.com>
2018-07-31 19:14:20 -0400

    Update Main.js

--- Geometry-Dash-AI/Main.js
+++ Geometry-Dash-AI/Main.js
@@ -7,10 +7,11 @@ function setup() {
 	Level = new LevelFunction();
 	GenerateLevel(20, 600, height/2+200, 180);
 	for (var i=0; i<30; i++) {
-	var Random = floor(random(0, 20));
-	if (Random <= 10) {Level[i] = 0;}
-	else if (Random <= 15 && Random >=11) {Level[i] = 1;}
-	else if (Random >= 16 && Random <=20) {Level[i] = 2;}
+		var Random = floor(random(0, 20));
+		if (Random <= 10) {Level[i] = 0;}
+		else if (Random <= 15 && Random >=11) {Level[i] = 1;}
+		else if (Random >= 16 && Random <=20) {Level[i] = 2;}
+	}
 }
 
 function draw() {
